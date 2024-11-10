4aaec9111c5b04f64a211d06ccf0d0adf13b6f82
muhammadhamza78 <160543204+muhammadhamza78@users.noreply.github.com>
2024-11-10 10:39:34 +0500

    Update Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="container">
         <h1>Welcome to My Portfolio</h1>
         <p>
-          Hi, I'm a Graphic Designer, Digital Marketer, and Front-End Developer. 
+          Hi, I am a Graphic Designer, Digital Marketer, and Front-End Developer. 
           This is where I showcase my work and expertise. Feel free to explore my 
           portfolio to learn more about my skills and projects.
         </p>
