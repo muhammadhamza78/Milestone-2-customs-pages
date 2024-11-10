aaa866e3b2cece1b80fd10e2cd42df5e525792ef
muhammadhamza78 <160543204+muhammadhamza78@users.noreply.github.com>
2024-11-10 10:28:47 +0500

    Update Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,18 +1,18 @@
 
-import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div>
-      <main className="container">
+      <div className="container">
         <h1>Welcome to My Portfolio</h1>
         <p>
           Hi, I'm a Graphic Designer, Digital Marketer, and Front-End Developer. 
           This is where I showcase my work and expertise. Feel free to explore my 
           portfolio to learn more about my skills and projects.
         </p>
-      </main>
-      <Footer />
+      </div>
+      <Navbar />
     </div>
   );
 };
