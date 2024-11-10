6a97afa9d34de9393de6564ecf75e84fa702a331
muhammadhamza78 <160543204+muhammadhamza78@users.noreply.github.com>
2024-11-10 10:38:51 +0500

    Update About.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,11 +4,11 @@ const About = () => {
       <div className="container">
         <h1>About Me</h1>
         <p>
-          Hello! I'm a passionate Graphic Designer, Digital Marketer, and Front-End Developer. 
+          Hello! I am a passionate Graphic Designer, Digital Marketer, and Front-End Developer. 
           I specialize in creating visually appealing designs, optimizing marketing strategies, 
           and building engaging user interfaces. My goal is to combine creativity with technology 
           to help businesses thrive in the digital world. 
-          I'm always open to new challenges and opportunities that allow me to grow and innovate.
+          I am always open to new challenges and opportunities that allow me to grow and innovate.
         </p>
       </div>
       <Footer />
