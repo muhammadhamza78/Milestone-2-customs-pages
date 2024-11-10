f18e310ce0bf186cc45a98df80d7351af9304baa
muhammadhamza78 <160543204+muhammadhamza78@users.noreply.github.com>
2024-11-10 10:25:31 +0500

    Update About.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,10 +1,7 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-
 const About = () => {
   return (
     <div>
-      <main className="container">
+      <div className="container">
         <h1>About Me</h1>
         <p>
           Hello! I'm a passionate Graphic Designer, Digital Marketer, and Front-End Developer. 
@@ -13,7 +10,7 @@ const About = () => {
           to help businesses thrive in the digital world. 
           I'm always open to new challenges and opportunities that allow me to grow and innovate.
         </p>
-      </main>
+      </div>
       <Footer />
     </div>
   );
